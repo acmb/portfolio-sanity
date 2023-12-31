b0a0e7087f59af1cf5a78ab96d0612fdebe7208b
RejaurRahman <rejaur-rahman@hotmail.co.uk>
2023-12-31 23:52:29 +0000

    ADD: logo and navbar for sanity cms studio

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,6 +4,9 @@ import { portfolioTheme } from "./theme"
 import { schemaTypes } from "./schemas"
 import { visionTool } from "@sanity/vision"
 
+import Logo from "@/components/Studio/Logo/Logo.component"
+import StudioNavbar from "@/components/Studio/StudioNavbar/StudioNavbar.component"
+
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!
 
@@ -18,6 +21,12 @@ export default defineConfig({
   projectId,
   schema: {
     types: schemaTypes
+  },
+  studio: {
+    components: {
+      logo: Logo,
+      navbar: StudioNavbar
+    }
   },
   theme: portfolioTheme,
   title: "PORTFOLIO Content Studio"
