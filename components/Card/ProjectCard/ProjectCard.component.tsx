15c7bf820228340a93c28d6d9a7a96832b859209
RejaurRahman <rejaur-rahman@hotmail.co.uk>
2023-12-31 15:58:44 +0000

    ADD: projects modal card

--- components/Card/ProjectCard/ProjectCard.component.tsx
+++ components/Card/ProjectCard/ProjectCard.component.tsx
@@ -1,3 +1,6 @@
+"use client"
+
+import * as Dialog from "@radix-ui/react-dialog"
 import Image from "next/image"
 import Link from "next/link"
 import React from "react"
@@ -5,15 +8,20 @@ import React from "react"
 import { CodeBracketIcon } from "@heroicons/react/24/solid"
 import { EyeIcon } from "@heroicons/react/24/solid"
 
+import ModalCard from "../ModalCard/ModalCard.component"
+
 import styles from "./ProjectCard.module.scss"
 
 type ProjectCardProps = {
+  backgroundText: string
   brandColor: string
   coverImage: string
-  github: {
+  futureUseText?: string
+  github?: {
     display: boolean
     url: string
   }
+  skills?: string[]
   text: string
   title: string
   website: {
@@ -23,12 +31,15 @@ type ProjectCardProps = {
 }
 
 export default function ProjectCard({
+  backgroundText,
   brandColor,
   coverImage,
+  futureUseText,
   github,
+  skills,
   text,
   title,
-  website,
+  website
 }: ProjectCardProps) {
   return (
     <div
@@ -69,35 +80,54 @@ export default function ProjectCard({
           className="flex justify-between py-2.5 border-b border-solid"
           style={{ borderColor: `${brandColor}` }}
         >
-          <div className="flex">
-            {github && github.url && (
-              <Link
-                aria-label="Github project profile page"
-                className={styles.iconLink}
-                href={github.url}
-                rel="noopener"
-                target="_blank"
-              >
-                <CodeBracketIcon
-                  className="w-8 h-8"
-                  style={{ fill: `${brandColor}` }}
-                />
-              </Link>
-            )}
-            {website && website.url && (
-              <Link
-                aria-label={`${title} page URL`}
-                className={styles.iconLink}
-                href={website.url}
-                rel="noopener"
-                target="_blank"
-              >
-                <EyeIcon
-                  className="w-8 h-8"
-                  style={{ fill: `${brandColor}` }}
-                />
-              </Link>
-            )}
+          <div className="flex justify-between w-full">
+            <Dialog.Root>
+              <Dialog.Trigger asChild>
+                <button
+                  className={`uppercase p-2.5 rounded-lg ${styles.button}`}
+                  style={{ color: `${brandColor}` }}
+                >
+                  Project Info
+                </button>
+              </Dialog.Trigger>
+              <ModalCard
+                backgroundText={backgroundText}
+                futureUseText={futureUseText}
+                modalLink={website.url}
+                modalTitle={title}
+                skills={skills}
+              />
+            </Dialog.Root>
+            <div className="flex">
+              {github && github.url && (
+                <Link
+                  aria-label="Github project profile page"
+                  className={styles.iconLink}
+                  href={github.url}
+                  rel="noopener"
+                  target="_blank"
+                >
+                  <CodeBracketIcon
+                    className="w-8 h-8"
+                    style={{ fill: `${brandColor}` }}
+                  />
+                </Link>
+              )}
+              {website && website.url && (
+                <Link
+                  aria-label={`${title} page URL`}
+                  className={styles.iconLink}
+                  href={website.url}
+                  rel="noopener"
+                  target="_blank"
+                >
+                  <EyeIcon
+                    className="w-8 h-8"
+                    style={{ fill: `${brandColor}` }}
+                  />
+                </Link>
+              )}
+            </div>
           </div>
         </div>
       </div>
--- regions/Projects/Projects.component.tsx
+++ regions/Projects/Projects.component.tsx
@@ -33,70 +33,104 @@ export default function Projects() {
             className="flex justify-center items-start flex-wrap"
           >
             <ProjectCard
-              brandColor="#ca9300"
+              backgroundText="React Shopping to do list project where you can add shopping items. Once shopping items are added they can be filtered by categories as well delete them. Users have option to complete items. Data is stored in firebase."
+              brandColor="#800517"
               coverImage="https://loremflickr.com/767/500"
+              futureUseText="Allow users to create list, which will be the first screen and then users can add shopping items. Users can archive list and view archived list."
               github={{
                 display: true,
                 url: "https://github.com/RejaurRahman/blog-camp"
               }}
-              text="Next.js + Sanity.io Blog"
-              title="Blog Camp"
+              skills={[
+                "React",
+                "Stripe",
+                "Firebase",
+                "Test 1"
+              ]}
+              text="Next.js + Sanity.io Blog 1"
+              title="Blog Camp 1"
               website={{
                 display: true,
                 url: "https://blog-camp.netlify.app/"
               }}
             />
             <ProjectCard
-              brandColor="#ca9300"
+              backgroundText="Restaurant ecommerce website integrated Stripe for payment and food items stored in Firebase Database, as well as Redux for centralized state management."
+              brandColor="#4a0fcb"
               coverImage="https://loremflickr.com/767/500"
               github={{
                 display: true,
                 url: "https://github.com/RejaurRahman/blog-camp"
               }}
-              text="Next.js + Sanity.io Blog"
-              title="Blog Camp"
+              skills={[
+                "React",
+                "Stripe",
+                "Firebase",
+                "Test 2"
+              ]}
+              text="Next.js + Sanity.io Blog 2"
+              title="Blog Camp 2"
               website={{
                 display: true,
                 url: "https://blog-camp.netlify.app/"
               }}
             />
             <ProjectCard
+              backgroundText="Personal blog with ability to leave comment which displays after approval. Ability to add pages like About and Contact page with Form Builder component as well as search functionality."
               brandColor="#ca9300"
               coverImage="https://loremflickr.com/767/500"
-              github={{
-                display: true,
-                url: "https://github.com/RejaurRahman/blog-camp"
-              }}
-              text="Next.js + Sanity.io Blog"
-              title="Blog Camp"
+              futureUseText="Live-as-you-type previewing of draft CMS content."
+              skills={[
+                "React",
+                "Stripe",
+                "Firebase",
+                "Test 3"
+              ]}
+              text="Next.js + Sanity.io Blog 3"
+              title="Blog Camp 3"
               website={{
                 display: true,
                 url: "https://blog-camp.netlify.app/"
               }}
             />
             <ProjectCard
-              brandColor="#ca9300"
+              backgroundText="Vue.JS project used to create animal profiles which can be navigated through based on the Discover Zone allocated in data JSON file. Created accordion, mouse hover functionalities on images, Meta-data structure for Analytics purposes in future and many more using Vue.JS."
+              brandColor="#53900f"
               coverImage="https://loremflickr.com/767/500"
               github={{
                 display: true,
                 url: "https://github.com/RejaurRahman/blog-camp"
               }}
-              text="Next.js + Sanity.io Blog"
-              title="Blog Camp"
+              skills={[
+                "React",
+                "Stripe",
+                "Firebase",
+                "Test 4"
+              ]}
+              text="Next.js + Sanity.io Blog 4"
+              title="Blog Camp 4"
               website={{
                 display: true,
                 url: "https://blog-camp.netlify.app/"
               }}
             />
             <ProjectCard
-              brandColor="#ca9300"
+              backgroundText="Boostrap CSS Framework used to create e-commerce html restaurant website."
+              brandColor="#5680e9"
               coverImage="https://loremflickr.com/767/500"
+              futureUseText="Re-platform using Gatsby.JS (React Framework) and incorporate with Netlify as the CMS."
               github={{
                 display: true,
                 url: "https://github.com/RejaurRahman/blog-camp"
               }}
-              text="Next.js + Sanity.io Blog"
-              title="Blog Camp"
+              skills={[
+                "React",
+                "Stripe",
+                "Firebase",
+                "Test 5"
+              ]}
+              text="Next.js + Sanity.io Blog 5"
+              title="Blog Camp 5"
               website={{
                 display: true,
                 url: "https://blog-camp.netlify.app/"
