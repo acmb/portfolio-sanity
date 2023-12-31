6381979c88f540b5fee8e20607d4ee5bb70f221a
RejaurRahman <rejaur-rahman@hotmail.co.uk>
2023-12-31 14:40:22 +0000

    UPDATE: nav with active class state

--- components/Layout/HeaderMenu/HeaderMenu.component.tsx
+++ components/Layout/HeaderMenu/HeaderMenu.component.tsx
@@ -1,13 +1,19 @@
+"use client"
+
 import Link from "next/link"
-import React from "react"
+import React, { useState } from "react"
 
 import styles from "./HeaderMenu.module.scss"
 
 export default function HeaderMenu() {
+  const [activeSection, setActiveSection] = useState("")
+
   const scrollToSection = (section: string) => (
     e: React.MouseEvent<HTMLAnchorElement>
   ) => {
     e.preventDefault()
+
+    setActiveSection(section)
 
     const targetSection = document.querySelector(`[data-position="${section}"]`) as HTMLElement | null
 
@@ -25,7 +31,7 @@ export default function HeaderMenu() {
         <li>
           <Link
             aria-label="View home section of page"
-            className={`flex capitalize box-content ${styles.link}`}
+            className={`flex capitalize box-content ${styles.link} ${activeSection === "home" ? styles.linkActive : ""}`.trim()}
             href="#home"
             onClick={scrollToSection("home")}
           >
@@ -35,7 +41,7 @@ export default function HeaderMenu() {
         <li>
           <Link
             aria-label="View about section of page"
-            className={`flex capitalize box-content ${styles.link}`}
+            className={`flex capitalize box-content ${styles.link} ${activeSection === "about" ? styles.linkActive : ""}`.trim()}
             href="#about"
             onClick={scrollToSection("about")}
           >
@@ -45,7 +51,7 @@ export default function HeaderMenu() {
         <li>
           <Link
             aria-label="View education section of page"
-            className={`flex capitalize box-content ${styles.link}`}
+            className={`flex capitalize box-content ${styles.link} ${activeSection === "education" ? styles.linkActive : ""}`.trim()}
             href="#education"
             onClick={scrollToSection("education")}
           >
@@ -55,7 +61,7 @@ export default function HeaderMenu() {
         <li>
           <Link
             aria-label="View experience section of page"
-            className={`flex capitalize box-content ${styles.link}`}
+            className={`flex capitalize box-content ${styles.link} ${activeSection === "experience" ? styles.linkActive : ""}`.trim()}
             href="#experience"
             onClick={scrollToSection("experience")}
           >
@@ -65,7 +71,7 @@ export default function HeaderMenu() {
         <li>
           <Link
             aria-label="View skillset section of page"
-            className={`flex capitalize box-content ${styles.link}`}
+            className={`flex capitalize box-content ${styles.link} ${activeSection === "skillset" ? styles.linkActive : ""}`.trim()}
             href="#skillset"
             onClick={scrollToSection("skillset")}
           >
@@ -75,7 +81,7 @@ export default function HeaderMenu() {
         <li>
           <Link
             aria-label="View projects section of page"
-            className={`flex capitalize box-content ${styles.link}`}
+            className={`flex capitalize box-content ${styles.link} ${activeSection === "projects" ? styles.linkActive : ""}`.trim()}
             href="#projects"
             onClick={scrollToSection("projects")}
           >
@@ -85,7 +91,7 @@ export default function HeaderMenu() {
         <li>
           <Link
             aria-label="View testimonials section of page"
-            className={`flex capitalize box-content ${styles.link}`}
+            className={`flex capitalize box-content ${styles.link} ${activeSection === "testimonials" ? styles.linkActive : ""}`.trim()}
             href="#testimonials"
             onClick={scrollToSection("testimonials")}
           >
@@ -95,7 +101,7 @@ export default function HeaderMenu() {
         <li>
           <Link
             aria-label="View contact section of page"
-            className={`flex capitalize box-content ${styles.link}`}
+            className={`flex capitalize box-content ${styles.link} ${activeSection === "contact" ? styles.linkActive : ""}`.trim()}
             href="#contact"
             onClick={scrollToSection("contact")}
           >
