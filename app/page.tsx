7223342dd093f1d57d396b7b0703c9d874c8143f
RejaurRahman <rejaur-rahman@hotmail.co.uk>
2023-12-31 13:58:49 +0000

    ADD: about us region

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import React from "react"
 
+import About from "@/regions/About/About.component"
 import Intro from "@/regions/Intro/Intro.component"
 
 export default function Home() {
   return (
-    <Intro />
+    <>
+      <About />
+      <Intro />
+    </>
   )
 }
---
+++ regions/About/About.component.tsx
@@ -0,0 +1,26 @@
+import React from "react"
+
+import { LightBulbIcon } from "@heroicons/react/24/solid"
+
+import Container from "@/components/Container/Container.component"
+import HeadingDivider from "@/components/HeadingDivider/HeadingDivider.component"
+import Section from "@/components/Section/Section.component"
+
+import styles from "./About.module.scss"
+
+export default function About() {
+  return (
+    <Section sectionClassName={`pt-12 ${styles.wrapper}`}>
+      <HeadingDivider
+        dividerLineBg="secondary"
+        icon={<LightBulbIcon />}
+        title="About Myself"
+      />
+      <Container
+        containerClassName={styles.containerWrapper}
+      >
+        <p>About</p>
+      </Container>
+    </Section>
+  )
+}
