7d50ea769c7ad241d7bb5520ff486830f57df8e6
RejaurRahman <rejaur-rahman@hotmail.co.uk>
2023-12-31 14:47:37 +0000

    ADD: markup and styles for about region

--- regions/About/About.component.tsx
+++ regions/About/About.component.tsx
@@ -1,7 +1,9 @@
+import Image from "next/image"
 import React from "react"
 
 import { LightBulbIcon } from "@heroicons/react/24/solid"
 
+import Badge from "@/components/Badge/Badge.component"
 import Container from "@/components/Container/Container.component"
 import HeadingDivider from "@/components/HeadingDivider/HeadingDivider.component"
 import Section from "@/components/Section/Section.component"
@@ -22,7 +24,23 @@ export default function About() {
       <Container
         containerClassName={styles.containerWrapper}
       >
-        <p>About</p>
+        <div className={styles.left}>
+          <p>People also know me as <span className={`font-bold ${styles.highlighted}`}>RONNY</span>.</p>
+          <p><span className={`font-bold ${styles.highlighted}`}>labor omnia vincit</span> is latin phrase written which means <span>hardwork conquers all</span> which I truly believe and follow in every day to day activities.</p>
+          <p>This has helped me build my repertoire and progress as <span className={`font-bold ${styles.highlighted}`}>Front End Web Developer</span>.</p>
+          <p>By gaining <span className={`font-bold ${styles.highlighted}`}>over <span className={`font-bold ${styles.highlighted}`}></span>+ years</span> commercial experience I have always shown to be a enthusiastic Developer with the eagerness to improve my current skillset, as well as going outside my comfort zone and finding creative solutions to any problems.</p>
+        </div>
+        <div className={styles.right}>
+          <Badge />
+          <Image
+            alt="Rejaur Rahman"
+            className=""
+            height={458}
+            loading="lazy"
+            src="https://loremflickr.com/592/458"
+            width={592}
+          />
+        </div>
       </Container>
     </Section>
   )
