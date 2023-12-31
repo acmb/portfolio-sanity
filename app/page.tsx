d6945078f93fe84ed8e146af4db618ca25b00902
RejaurRahman <rejaur-rahman@hotmail.co.uk>
2023-12-31 14:02:32 +0000

    ADD: education region

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 
 import About from "@/regions/About/About.component"
+import Education from "@/regions/Education/Education.component"
 import Intro from "@/regions/Intro/Intro.component"
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
     <>
       <Intro />
       <About />
+      <Education />
     </>
   )
 }
---
+++ regions/Education/Education.component.tsx
@@ -0,0 +1,30 @@
+import React from "react"
+
+import { BookOpenIcon } from "@heroicons/react/24/solid"
+
+import Container from "@/components/Container/Container.component"
+import HeadingDivider from "@/components/HeadingDivider/HeadingDivider.component"
+import InnerSection from "@/components/InnerSection/InnerSection.component"
+import Section from "@/components/Section/Section.component"
+
+import styles from "./Education.module.scss"
+
+export default function Education() {
+  return (
+    <Section>
+      <HeadingDivider
+        background
+        dividerLineBg="primary"
+        icon={<BookOpenIcon />}
+        title="Education Timeline"
+      />
+      <InnerSection
+        innerContentClass={styles.wrapper}
+      >
+        <Container>
+          <p>Education</p>
+        </Container>
+      </InnerSection>
+    </Section>
+  )
+}
