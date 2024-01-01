1b12a1aa550b125286433208cea7557b5b94e646
RejaurRahman <rejaur-rahman@hotmail.co.uk>
2024-01-01 01:18:43 +0000

    ADD: testimonial schema

--- schemas/index.ts
+++ schemas/index.ts
@@ -2,10 +2,12 @@ import experience from "./experience"
 import project from "./project"
 import sitewide from "./sitewide"
 import skill from "./skill"
+import testimonial from "./testimonial"
 
 export const schemaTypes = [
   experience,
   project,
   skill,
-  sitewide
+  sitewide,
+  testimonial
 ]
