8c164fb96df38c0c543def9124a2070c39468a0f
RejaurRahman <rejaur-rahman@hotmail.co.uk>
2024-01-02 00:01:56 +0000

    ADD: use sticky hook for for navigation use

--- components/App/Layout/Header/Header.component.tsx
+++ components/App/Layout/Header/Header.component.tsx
@@ -3,6 +3,8 @@
 import * as NavigationMenu from "@radix-ui/react-navigation-menu"
 import React from "react"
 
+import useSticky from "@/hooks/useSticky"
+
 import Container from "@/components/App/Container/Container.component"
 import HeaderLogo from "@/components/App/Layout/HeaderLogo/HeaderLogo.component"
 import HeaderMenu from "@/components/App/Layout/HeaderMenu/HeaderMenu.component"
@@ -11,9 +13,12 @@ import HeaderModal from "@/components/App/Layout/HeaderModal/HeaderModal.compone
 import styles from "./Header.module.scss"
 
 export default function Header() {
+  const { sticky, stickyRef } = useSticky()
+
   return (
     <header
-      className={`w-full top-0 z-50 ${styles.header}`}
+      className={`w-full top-0 z-50 ${styles.header} ${sticky ? styles.sticky : ""}`.trim()}
+      ref={stickyRef}
     >
       <NavigationMenu.Root>
         <Container
