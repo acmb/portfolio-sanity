084b2d358349191e6b61b359e97104dfc49dc68b
RejaurRahman <rejaur-rahman@hotmail.co.uk>
2024-01-08 02:33:59 +0000

    UPDATE: contact form to integrate with formspark

--- components/App/Form/Form.component.tsx
+++ components/App/Form/Form.component.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { SubmitPayload } from "@formspark/use-formspark"
 import {
   SubmitHandler,
   useForm
@@ -21,61 +22,94 @@ type Inputs = {
 type Props = {}
 
 export default function Contact({}: Props) {
+  const [submitted, setSubmitted] = useState(false)
   const [clientRendered, setClientRendered] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const { register, handleSubmit } = useForm()
 
-  const {
-    handleSubmit,
-    register
-  } = useForm<Inputs>()
-  const onSubmit: SubmitHandler<Inputs> = formData => {
-    window.location.href = `mailto:rejaur-rahman@hotmail.co.uk?subject=${formData.subject}&body=Hi my name is ${formData.name}. ${formData.message}`
+  const FORMSPARK_ACTION_URL = "https://submit-form.com/JWRilWPLQ"
+
+  const onSubmit: SubmitHandler<SubmitPayload> = async (data) => {
+    setLoading(true)
+
+    await fetch(FORMSPARK_ACTION_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        data
+      })
+    })
+    setSubmitted(true)
+    setLoading(false)
   }
 
   useEffect(() => {
     setClientRendered(true)
   }, [])
 
+  if (submitted) {
+    return (
+      <div
+        className={`flex w-full mt-7 mb-12 py-10 px-4 text-center ${styles.successBox}`}
+      >
+        <h3
+          className="text-3xl font-bold pb-1.5"
+        >
+          Thank you for submitting your message!
+        </h3>
+        <p>Someone from our team will reach out to you within 48 hours.</p>
+      </div>
+    )
+  }
+
   return (
     <>
-      <p className={`text-center mb-7 font-light ${styles.copy}`}>
-        If you need to get in contact with me, Let&apos;s talk
-      </p>
       {clientRendered && (
-        <form
-          className={`flex w-full ${styles.form}`}
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          <div className={`flex ${styles.group}`}>
-            <input
-              className={`mb-4 p-6 font-light rounded-lg ${styles.input}`}
-              placeholder="Name"
-              type="text"
-              {...register("name")}
-            />
-            <input
-              className={`mb-4 p-6 font-light rounded-lg ${styles.input}`}
-              placeholder="Email"
-              type="email"
-              {...register("email")}
-            />
-          </div>
-          <input
-            className={`mb-4 p-6 font-light rounded-lg ${styles.input}`}
-            placeholder="subject"
-            type="text"
-            {...register("subject")}
-          />
-          <textarea
-            className={`mb-4 p-6 font-light rounded-lg ${styles.textarea}`}
-            placeholder="Message"
-            {...register("message")}
-          />
-          <button
-            className={`p-4 font-light rounded-lg ${styles.button}`}
+        <>
+          <p className={`text-center mb-7 font-light ${styles.copy}`}>
+            If you need to get in contact with me, Let&apos;s talk
+          </p>
+          <form
+            className={`flex w-full ${styles.form}`}
+            onSubmit={handleSubmit(onSubmit)}
           >
-            Submit
-          </button>
-        </form>
+            <div className={`flex ${styles.group}`}>
+              <input
+                className={`mb-4 p-6 font-light rounded-lg ${styles.input}`}
+                placeholder="Name"
+                type="text"
+                {...register("name")}
+              />
+              <input
+                className={`mb-4 p-6 font-light rounded-lg ${styles.input}`}
+                placeholder="Email"
+                type="email"
+                {...register("email")}
+              />
+            </div>
+            <input
+              className={`mb-4 p-6 font-light rounded-lg ${styles.input}`}
+              placeholder="subject"
+              type="text"
+              {...register("subject")}
+            />
+            <textarea
+              className={`mb-4 p-6 font-light rounded-lg ${styles.textarea}`}
+              placeholder="Message"
+              {...register("message")}
+            />
+            <button
+              className={`p-4 font-light rounded-lg ${styles.button}`}
+              disabled={loading}
+              type="submit"
+            >
+              Submit
+            </button>
+          </form>
+        </>
       )}
     </>
   )
