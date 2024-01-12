d2c18cdea838d0e5b5852425f50aab0d0ed22b8b
RejaurRahman <rejaur-rahman@hotmail.co.uk>
2024-01-12 13:28:50 +0000

    UPDATE: fonts to preload

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import localFont from "next/font/local"
 import React from "react"
 
 import type { Metadata } from "next"
@@ -5,6 +6,52 @@ import type { Metadata } from "next"
 import metaImage from "@/assets/images/rejaur-ronny-rahman.png"
 
 import "./globals.css"
+
+const figtree = localFont({
+  src: [
+    {
+      path: "../assets/fonts/Figtree-Light.woff2",
+      style: "normal",
+      weight: "300"
+    },
+    {
+      path: "../assets/fonts/Figtree-Regular.woff2",
+      style: "normal",
+      weight: "400"
+    }
+  ],
+  variable: "--font-figtree"
+})
+
+const sketchBold = localFont({
+  src: "../assets/fonts/SketchBlock-Bold.woff2",
+  style: "normal",
+  variable: "--font-sketch",
+  weight: "500"
+})
+
+const sothardjo = localFont({
+  src: "../assets/fonts/Sothardjo.woff2",
+  style: "normal",
+  variable: "--font-sothardjo",
+  weight: "300"
+})
+
+const ubuntu = localFont({
+  src: [
+    {
+      path: "../assets/fonts/Ubuntu-Medium.woff2",
+      style: "normal",
+      weight: "400"
+    },
+    {
+      path: "../assets/fonts/Ubuntu-Bold.woff2",
+      style: "normal",
+      weight: "700"
+    }
+  ],
+  variable: "--font-ubuntu"
+})
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://rejaurrahman.co.uk"),
@@ -28,7 +75,7 @@ export default function RootLayout({
 }) {
   return (
     <html
-      className="scroll-smooth"
+      className={`scroll-smooth ${sketchBold.variable} ${figtree.variable} ${ubuntu.variable} ${sothardjo.variable}`}
       lang="en"
       style={{ scrollBehavior: "smooth" }}
     >
